c08abaf814e3954c0e7b29908d82ec3d680523fd
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-07-27 12:23:20 -0300

    Added more comments

--- bot/text_process.py
+++ bot/text_process.py
@@ -22,7 +22,10 @@ def post_process(text_input:str) -> str:
     text_output = text_match[0] if text_match is not None else text_input
 
     # Remove line breaks and extraneous spaces
+    text_output = TAGS_REGEX.sub("", text_output)           # Remove XML tags that somehow ended in the output
     text_output = NEWLINE_REGEX.sub("\g<1>. ", text_output) # Add a period to the lines ending without it
+    text_output = text_output.replace("\n", " ")            # Replace line breaks by spaces
+    text_output = SPACES_REGEX.sub(" ", text_output)        # Replace consecutive spaces by a single space
 
     # Remove the @ so the bot do not tag anyone
     text_output = text_output.replace("@", "(at)")
