c641195c355c1171ca59c7fb2baebac403d15c9a
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-07-27 12:21:38 -0300

    Add a period to lines that does not end with it

--- bot/text_process.py
+++ bot/text_process.py
@@ -3,6 +3,7 @@ import re
 RESPONSE_REGEX = re.compile(r"(?s)[A-Z].+[.!?\n]")
 TAGS_REGEX = re.compile(r"<.+?>")
 SPACES_REGEX = re.compile(r"\s{2,}")
+NEWLINE_REGEX = re.compile(r"(\w)\n")
 
 def pre_process(text:str) -> str:
     """Filters the message before submitting it to the AI to respond."""
@@ -12,7 +13,7 @@ def post_process(text_input:str) -> str:
     """Filters the bot's response, so it begins and ends at a full sentence.
     Also it removes the tags that the bot sometimes outputs."""
     
-    global RESPONSE_REGEX, TAGS_REGEX, SPACES_REGEX
+    global RESPONSE_REGEX, TAGS_REGEX, SPACES_REGEX, NEWLINE_REGEX
     
     # Crop the output so it begins and end at a sentence
     # (we are considering that a sentence begins at a capital leter,
@@ -21,9 +22,7 @@ def post_process(text_input:str) -> str:
     text_output = text_match[0] if text_match is not None else text_input
 
     # Remove line breaks and extraneous spaces
-    text_output = TAGS_REGEX.sub("", text_output)
-    text_output = text_output.replace("\n", " ")
-    text_output = SPACES_REGEX.sub(" ", text_output)
+    text_output = NEWLINE_REGEX.sub("\g<1>. ", text_output) # Add a period to the lines ending without it
 
     # Remove the @ so the bot do not tag anyone
     text_output = text_output.replace("@", "(at)")
