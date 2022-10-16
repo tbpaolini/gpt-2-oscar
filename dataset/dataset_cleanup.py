b0435533271bce6e0a9a88c13642c4ce6aec4f49
Tiago Becerra Paolini <tpaolini@gmail.com>
2022-10-16 13:39:57 -0300

    More cleanup rules - The "Yes No" questions are now always replaced correctly (the word "Not" no longer gets replaced). - Replacing the words "jerk " and "moron" by "maroon".

--- dataset/dataset_cleanup.py
+++ dataset/dataset_cleanup.py
@@ -10,6 +10,8 @@ string_regex = re.compile(r'"(.+)"')        # Get the strings
 tokens_regex = re.compile(r'([%~$^\*]\d+_)')  # Get the text's markup
 spaces_regex = re.compile(r'(\s{2,})')   # Get sequences of 2 or more blank spaces
 ellipsis_regex = re.compile(r'^\.\.\.(.+?)\.\.\.$') # Get the lines that start AND end with 3 periods
+yes_no_regex = re.compile(r"#(?:Yes|No)\b")             # Get the "Yes" or "No" from the multiple choice dialog
+jerk_regex = re.compile(r"(?i)\b(?:jerk|moron)(s)?\b")  # Get the words "jerk" or "moron" (singular and plural)
 
 # Get the two variations of the rotating texts
 rotating_text_left  = re.compile(r'(?:@(.+?)@.+?@)')
@@ -27,14 +29,21 @@ for input_path in dataset_path.glob("*_ENUS.gml"):
 
     # Process each line of text
     output_path = Path(cleaned_path, input_path.stem + ".txt")
+    previous_line = ""
     with open(output_path, "wt") as output_file:
         for line in raw_lines:
-            clean_line = tokens_regex.sub("", line)     # Remove markup tokens from the text
+            # Skip repeated lines of text
+            if line == previous_line: continue
+            line = previous_line
             
-            clean_line = clean_line.replace(" #Yes", "")    # Remove the "Yes No" options from the string
-            clean_line = clean_line.replace(" #No", "")
-            clean_line = clean_line.replace("#", " ")       # Replace the game's newline character (#) by a space
-            
+            # Skip placeholder lines
+            if line.startswith("[PH]"): continue
+
+            # Perform text replacements
+            clean_line = tokens_regex.sub("", line)                 # Remove markup tokens from the text
+            clean_line = yes_no_regex.sub("", clean_line)           # Remove the "Yes No" options from the string
+            clean_line = jerk_regex.sub(r"maroon\g<1>", clean_line) # Replace the words "jerk" and "moron" by "maroon"
+            clean_line = clean_line.replace("#", " ")               # Replace the game's newline character (#) by a space
             clean_line = spaces_regex.sub(" ", clean_line).strip()  # Remove extraneous blank spaces
             clean_line = ellipsis_regex.sub(r"\g<1>.", clean_line)  # Remove the ellipsis at the beginning and end
             
